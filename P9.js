185b567957708699f4e2f9c88eb99e95f97034aa
Archi Misra <misraarchi117@gmail.com>
2026-03-05 22:47:15 +0530

    Add files via upload

---
+++ P9.js
@@ -0,0 +1,11 @@
+//create a function using the "function" keyword that takes a string as an argument and returns the number of vowels in the string
+function countVowels(str){
+    let count = 0;
+    //"archimisra"
+    for(const char of str){
+      if(char ==="a"||char ==="e"|| char === "i"|| char === "o"|| char === "u"){
+            count++;
+      }
+    }
+    console.log(count);
+}
